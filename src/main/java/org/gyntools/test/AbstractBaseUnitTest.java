a8f0b89410ff69183be645e82e2d360af3d1de86
Rafael Pereira <rafaelsouza@sistemafieg.org.br>
2016-08-16 14:21:15 -0300

    BaseUnitTest

---
+++ src/main/java/org/gyntools/test/AbstractBaseUnitTest.java
@@ -0,0 +1,16 @@
+package org.gyntools.test;
+
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Created by rafael on 8/16/16.
+ */
+public abstract class AbstractBaseUnitTest {
+
+    @BeforeMethod(alwaysRun=true)
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+}
